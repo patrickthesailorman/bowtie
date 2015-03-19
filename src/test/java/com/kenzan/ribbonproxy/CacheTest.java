4b569c8d1ccda052690539c05f7bf9ee7421a7b8
Jonathan Stockdill <jons@kenzanmedia.com>
2015-03-19 12:00:33 -0400

    Framework

---
+++ src/test/java/com/kenzan/ribbonproxy/CacheTest.java
@@ -0,0 +1,16 @@
+package com.kenzan.ribbonproxy;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class CacheTest {
+
+    @Test
+    public void test() {
+
+        fail("Not yet implemented");
+    }
+
+}
